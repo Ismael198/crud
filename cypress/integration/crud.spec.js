64965832301696dc09b88d49ff803e53b1e71115
Ismael Nunes Martins <ismael.nunes.martins@gmail.com>
2021-05-06 18:36:35 -0300

    commit mentoria

--- cypress/integration/crud.spec.js
+++ cypress/integration/crud.spec.js
@@ -7,23 +7,23 @@ describe('Notes', () => {
 
   it('creates a note', () => {
     cy.contains('Create a new note').click()
-    cy.get('#content').type('My note')
+    cy.get('#content').type('Meu notebook')
     cy.contains('Create').click()
-    cy.get('.list-group').should('contain', 'My note')
+    cy.get('.list-group').should('contain', 'Meu notebook')
   })
 
   it('edits a note', () => {
-    cy.get('.list-group').contains('My note').click()
+    cy.get('.list-group').contains('Meu notebook').click()
     cy.get('#content').type(' updated')
     cy.contains('Save').click()
-    cy.get('.list-group').should('contain', 'My note updated')
-    cy.get('.list-group:contains(My note updated)').should('be.visible')
+    cy.get('.list-group').should('contain', 'Meu notebook updated')
+    cy.get('.list-group:contains(Meu notebook updated)').should('be.visible')
   })
 
   it('deletes a note', () => {
-    cy.get('.list-group').contains('My note updated').click()
+    cy.get('.list-group').contains('Meu notebook updated').click()
     cy.contains('Delete').click()
 
-    cy.get('.list-group:contains(My note updated)').should('not.exist')
+    cy.get('.list-group:contains(Meu notebook updated)').should('not.exist')
   })
 })
